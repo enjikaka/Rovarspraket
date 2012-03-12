93d1fe0da40069344fa75961281eee3684ec5622
Fred Bergman <fred@fbergman.se>
2012-03-12 11:32:20 +0100

    removed C and c conversion

--- rovarspraket.js
+++ rovarspraket.js
@@ -2,7 +2,7 @@
 String.prototype.toRovarspraket = function () {
   
   var constants = { b: true, c: true, d: true, f: true, g: true, h: true, j: true, k: true, l: true, m: true, n: true, p: true, q: true, r: true, s: true, t: true, v: true, w: true, x: true, z: true },
-      str = this.toString().replace('x', 'ks').replace('X', 'Ks').replace('c', 'k').replace('C', 'K'),
+      str = this.toString().replace('x', 'ks').replace('X', 'Ks'),
       str_arr = str.split(''),
       sostotror = '',
       i,
