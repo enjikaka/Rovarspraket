0e415e260eeb5e03789803f3aa6531ab6060a228
Fred Bergman <fred@fbergman.se>
2012-03-13 17:37:33 +0100

    fromRovarspraket

--- rovarspraket.js
+++ rovarspraket.js
@@ -15,3 +15,11 @@ String.prototype.toRovarspraket = function () {
   
   return sostotror;
 };
+
+String.prototype.fromRovarspraket = function () {
+  
+  return this.toString().replace(/(b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|z)+o+(b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|z)/gi, function(str) {
+    return str.substr(0, 1);
+  });
+  
+};
