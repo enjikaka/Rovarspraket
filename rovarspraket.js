ac49a73f9576817c22bfb29e8b710436e38a7335
Fred Bergman <fred@fbergman.se>
2012-03-13 17:37:59 +0100

    fixed typo

--- rovarspraket.js
+++ rovarspraket.js
@@ -1,7 +1,7 @@
 // http://en.wikipedia.org/wiki/R%C3%B6varspr%C3%A5ket
 String.prototype.toRovarspraket = function () {
   
-  var constants = { b: true, c: true, d: true, f: true, g: true, h: true, j: true, k: true, l: true, m: true, n: true, p: true, q: true, r: true, s: true, t: true, v: true, w: true, x: true, z: true },
+  var consonants = { b: true, c: true, d: true, f: true, g: true, h: true, j: true, k: true, l: true, m: true, n: true, p: true, q: true, r: true, s: true, t: true, v: true, w: true, x: true, z: true },
       str = this.toString().replace('x', 'ks').replace('X', 'Ks'),
       str_arr = str.split(''),
       sostotror = '',
@@ -10,7 +10,7 @@ String.prototype.toRovarspraket = function () {
   
   for (i = 0; i < str_arr.length; i++) {
     character = str_arr[i];
-    sostotror += constants[character.toLowerCase()] ? character + 'o' + character.toLowerCase() : character;
+    sostotror += consonants[character.toLowerCase()] ? character + 'o' + character.toLowerCase() : character;
   }
   
   return sostotror;
