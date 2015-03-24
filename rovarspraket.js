4ba12b1dcc7370bf439b3d0b8285d6ce01653eec
Jeremy Karlsson <hello@jeremy.se>
2015-03-24 01:34:41 +0100

    Made "class" instead of extending photo

--- rovarspraket.js
+++ rovarspraket.js
@@ -1,16 +1,14 @@
 // http://en.wikipedia.org/wiki/R%C3%B6varspr%C3%A5ket
-String.prototype.toRovarspraket = function () {
-  
-  return this.toString().replace(/(b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|z)/gi, function(str) {
-    return str+'o'+str.toLowerCase();
-  });
-  
-};
 
-String.prototype.fromRovarspraket = function () {
-  
-  return this.toString().replace(/(b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|z)+o+(b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|z)/gi, function(str) {
-    return str.substr(0, 1);
-  });
-  
+var Rovarspraket = {
+	to: function(s) {
+		return s.toString().replace(/(b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|z)/gi, function(str) {
+			return str + 'o' + str.toLowerCase();
+		});
+	},
+	from: function(s) {
+		return s.toString().replace(/(b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|z)+o+(b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|z)/gi, function(str) {
+			return str.substr(0, 1);
+		});
+	}
 };
